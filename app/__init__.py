886df09ab74301f27e3ecc5e0858e72b1810d346
Tom Chege <emdeechege@gmail.com>
2019-01-09 07:27:42 +0300

    [start#163046360] adds meetup model and views files

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,16 @@
 '''Creating app'''
 import os
 from flask import Flask
-from instance.config import app_config
-"""importing the configurations from the .config file which is in the instance folder"""
+from app.api.v1.views.meetup_views import v1_meetup_blueprint
+# from instance.config import app_config
+# """importing the configurations from the .config file which is in the instance folder"""
 
 
 def create_app():
     '''creating  the app using the configurations in the dictionary created in the .config file'''
     app = Flask(__name__)
-
+    # register blueprints
+    app.register_blueprint(v1_meetup_blueprint)
     # Creating the app configurations
     # app.config.from_object(app_config[config_name])
     # app.config.from_pyfile('config.py')
