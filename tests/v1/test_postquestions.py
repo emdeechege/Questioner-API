9021041e594e842464586da6cd910eb7a4adff05
Tom Chege <emdeechege@gmail.com>
2019-01-10 06:57:07 +0300

    [start#163108855] added questions testfile

---
+++ tests/v1/test_postquestions.py
@@ -0,0 +1,27 @@
+import unittest
+from app import create_app
+import json
+
+
+class TestQuestions(unittest.TestCase):
+    '''Test Question class'''
+
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client()
+        self.question = {
+            "postedBy": "user_id",
+            "meetup_id": "meetup_id",
+            "title": "Why now?",
+            "content": "True or false",
+            "votes": "votes"
+        }
+
+    def tearDown(self):
+        del self.question
+
+    def test_post question(self):
+        question = self.client.post(
+            'api/v1/meetups', data=json.dumps(self.question), content_type='application/json')
+
+        self.assertEqual(meet.status_code, 201)
