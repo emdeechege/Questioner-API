9bd52f11abcff905484c8606aaaeaf39c1fc6d2d
Tom Chege <emdeechege@gmail.com>
2019-01-11 10:07:54 +0300

    [start#163046249] added voting to questions test

--- tests/v1/test_postquestions.py
+++ tests/v1/test_postquestions.py
@@ -11,10 +11,25 @@ class TestQuestions(unittest.TestCase):
         self.client = self.app.test_client()
         self.question = {
             "postedBy": "user_id",
-            "meetup_id": "meetup_id",
+            "question_id": "question_id",
             "title": "Why now?",
             "content": "True or false",
-            "votes": "votes"
+
+        }
+        self.upvote = {
+            "postedBy": "user_id",
+            "question_id": "question_id",
+            "title": "Why now?",
+            "content": "True or false",
+            "votes": 1
+        }
+
+        self.downvote = {
+            "postedBy": "user_id",
+            "question_id": "question_id",
+            "title": "Why now?",
+            "content": "True or false",
+            "votes": 1
         }
 
     def tearDown(self):
@@ -25,3 +40,40 @@ class TestQuestions(unittest.TestCase):
             'api/v1/questions', data=json.dumps(self.question), content_type='application/json')
 
         self.assertEqual(question.status_code, 201)
+
+    def test_all_questions_fetch(self):
+        all_questions = self.client.get("api/v1/questions")
+
+        result = json.loads(all_questions.data.decode())
+        self.assertEqual(result["message"], "Success")
+        self.assertEqual(all_questions.status_code, 200)
+
+    def test_getone_question(self):
+        # post one question
+        question = self.client.post(
+            'api/v1/questions', data=json.dumps(self.question), content_type='application/json')
+
+        response = self.client.get("api/v1/questions/1")
+        res = json.loads(response.data.decode())
+        self.assertEqual(res["message"], "Success")
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_no_question(self):
+        """ tests when question id does not exist """
+        response = self.client.get("api/v1/questions/6")
+        res = json.loads(response.data.decode())
+        self.assertEqual(res["message"], "question not found")
+        self.assertEqual(response.status_code, 404)
+
+    def test_upvote(self):
+
+        upvote = self.client.patch('/api/v1/questions/1/upvote',
+                                   data=json.dumps(self.upvote), content_type='application/json')
+
+        self.assertEqual(upvote.status_code, 201)
+
+    def test_downvote(self):
+        downvote = self.client.patch('/api/v1/questions/1/downvote',
+                                     data=json.dumps(self.downvote), content_type='application/json')
+
+        self.assertEqual(downvote.status_code, 201)
