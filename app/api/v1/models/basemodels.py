97c8dc55092c4afe5b2a04ac90d86f8aa101904b
Tom Chege <emdeechege@gmail.com>
2019-01-15 13:25:59 +0300

    [start#163216630] fixes failing tests

--- app/api/v1/models/basemodels.py
+++ app/api/v1/models/basemodels.py
@@ -2,6 +2,6 @@ class BaseModels(object):
     """ contains methods common to other models """
 
     def check_item(self, item, key, db):
-        """checks for data in dictionaries"
+        """checks for data in dictionaries"""
         data = [record for record in db if record[key] == item]
         return data
