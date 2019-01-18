4b33a7308a64efb22a15392af665ca7ba379b8b3
Tom Chege <emdeechege@gmail.com>
2019-01-18 09:36:43 +0300

    [feature#163278985] made changes to table properties to fit characteristics of potential field

--- app/connect.py
+++ app/connect.py
@@ -27,7 +27,7 @@ def test_init_db():
 
 def create_tables(conn):
     curr = conn.cursor()
-    queries = tables()
+    queries = schema.tables()
 
     for query in queries:
         curr.execute(query)
@@ -35,7 +35,7 @@ def create_tables(conn):
 
 def destroy():
     """tear down for db during after tests"""
-    
+
     conn = psycopg2.connect("dbname=questioner_test user=emdee password=arif@123")
     curr = conn.cursor()
     users = "DROP TABLE IF EXISTS users CASCADE"
@@ -50,53 +50,3 @@ def destroy():
         conn.commit()
     except:
         return "Tables not dropped"
-
-""" holds database information in tabular format"""
-def tables():
-    """ defines database table structures"""
-
-    users = """CREATE TABLE IF NOT EXISTS users(
-        id serial PRIMARY KEY NOT NULL,
-        firstname character varying(50) NOT NULL,
-        lastname  character varying(50) NOT NULL,
-        othername character varying(50) NOT NULL,
-        email  character varying(50) UNIQUE,
-        phoneNumber numeric NOT NULL,
-        username character varying(50) NOT NULL,
-        password character varying(50) NOT NULL,
-        isAdmin BOOLEAN NOT NULL DEFAULT FALSE,
-        registered timestamp default current_timestamp
-    );"""
-
-    meetups = """CREATE TABLE IF NOT EXISTS meetups(
-        id serial PRIMARY KEY NOT NULL,
-        happenningOn date NOT NULL,
-        location character varying(50) NULL,
-        images text NULL,
-        topic character varying(200) NOT NULL,
-        tags text NULL,
-        createdOn timestamp default current_timestamp
-    );"""
-
-
-    questions = """CREATE TABLE IF NOT EXISTS questions(
-        id serial PRIMARY KEY NOT NULL,
-        meetup_id numeric NOT NULL,
-        user_id numeric NOT NULL,
-        postedBy numeric NOT NULL,
-        title character varying(200) NOT NULL,
-        body text NOT NULL,
-        votes integer DEFAULT 0,
-        createdOn timestamp with time zone DEFAULT ('now'::text)::date NOT NULL
-    );"""
-
-    rsvps = """CREATE TABLE IF NOT EXISTS rsvps(
-        id serial PRIMARY KEY NOT NULL,
-        meetup_id numeric NOT NULL,
-        user_id numeric NOT NULL,
-        response character varying(30) NOT NULL
-    );"""
-
-    queries = [users, meetups, questions, rsvps]
-
-    return queries
--- app/schema.py
+++ app/schema.py
@@ -1,37 +1,38 @@
 """ holds database information in tabular format"""
+
 def tables():
     """ defines database table structures"""
 
     users = """CREATE TABLE IF NOT EXISTS users(
-        id serial PRIMARY KEY NOT NULL,
-        firstname varchar varying(50) NOT NULL,
-        lastname  varchar varying(50) NOT NULL,
-        othername varchar varying(50) NOT NULL,
-        email  varchar varying(50) UNIQUE,
-        phoneNumber numeric NOT NULL
-        username varchar varying(50) NOT NULL,
-        password varchar varying(50) NOT NULL,
-        isAdmin BOOLEAN NOT NULL DEFAULT FALSE
+        user_id serial PRIMARY KEY NOT NULL,
+        firstname character varying(50) NOT NULL,
+        lastname  character varying(50) NOT NULL,
+        othername character varying(50) NOT NULL,
+        email  character varying(70) UNIQUE,
+        phoneNumber numeric NOT NULL,
+        username character varying(50) NOT NULL,
+        password character varying(1000) NOT NULL,
+        isAdmin BOOLEAN NOT NULL DEFAULT FALSE,
         registered timestamp default current_timestamp
     );"""
 
-    meetups = """"CREATE TABLE IF NOT EXIST meetups(
-        id serial PRIMARY KEY NOT NULL,
+    meetups = """CREATE TABLE IF NOT EXISTS meetups(
+        meetup_id serial PRIMARY KEY NOT NULL,
         happenningOn date NOT NULL,
-        location varchar varying(50) NULL,
+        location character varying(50) NULL,
         images text NULL,
-        topic varchar varying(200) NOT NULL,
+        topic character varying(200) NOT NULL,
         tags text NULL,
         createdOn timestamp default current_timestamp
     );"""
 
 
     questions = """CREATE TABLE IF NOT EXISTS questions(
-        id serial PRIMARY KEY NOT NULL,
+        question_id serial PRIMARY KEY NOT NULL,
         meetup_id numeric NOT NULL,
         user_id numeric NOT NULL,
         postedBy numeric NOT NULL,
-        title varchar varying(200) NOT NULL,
+        title character varying(200) NOT NULL,
         body text NOT NULL,
         votes integer DEFAULT 0,
         createdOn timestamp with time zone DEFAULT ('now'::text)::date NOT NULL
@@ -41,9 +42,9 @@ def tables():
         id serial PRIMARY KEY NOT NULL,
         meetup_id numeric NOT NULL,
         user_id numeric NOT NULL,
-        response varchar varying(30) NOT NULL,
+        response character varying(30) NOT NULL
     );"""
 
-    tables = [users,meetups,questions,rsvps]
+    queries = [users, meetups, questions, rsvps]
 
-    return tables
+    return queries
