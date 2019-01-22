471541de40d8c135e000889f2166b766c197bb3c
Tom Chege <emdeechege@gmail.com>
2019-01-22 15:58:14 +0300

    [feature#163378824] registers meetup blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from .api.v1.views.meetup_views import v1_meetup_blueprint
 from .api.v1.views.questions_views import v1_question_blueprint
 from .api.v1.views.auth_views import v1_auth_blueprint
 from .api.v2.views.auth_views import v2_auth
+from .api.v2.views.meetup_views import v2_meetup
 
 
 def create_app(config_name):
@@ -14,7 +15,7 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
-    
+
 
     """register v1 blueprints"""
     app.register_blueprint(v1_meetup_blueprint)
@@ -22,6 +23,7 @@ def create_app(config_name):
     app.register_blueprint(v1_auth_blueprint)
 
     app.register_blueprint(v2_auth)
+    app.register_blueprint(v2_meetup)
 
 
     return app
