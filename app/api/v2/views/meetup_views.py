b2769a28893b7454219a549cfaead7180b639404
Tom Chege <emdeechege@gmail.com>
2019-01-22 15:54:11 +0300

    [feature#163378824] adds meetup model methods

--- app/api/v2/views/meetup_views.py
+++ app/api/v2/views/meetup_views.py
@@ -3,77 +3,39 @@ from datetime import datetime
 from ..models.meetups_models import Meetup, Rsvp
 
 
-v1_meetup_blueprint = Blueprint('meetups', __name__, url_prefix='/api/v1')
+v2_meetup = Blueprint('meetups', __name__, url_prefix='/api/v1')
 
 meetups = Meetup()
 rsvp = Rsvp()
 
 
-@v1_meetup_blueprint.route('/meetups', methods=['POST'])
+@v2_meetup.route('/meetups', methods=['POST'])
 def create_meetup():
     """ endpoint for creating meetup"""
 
     data = request.get_json()
     if not data:
         return jsonify({"message": "Data set cannot be empty"})
-    """ Check for empty inputs"""
-    if not all(field in data for field in ["title", "organizer", "images", "location", "happeningOn", "tags"]):
-        return jsonify({"status": 400, "message": "Please fill in all the required input fields"}), 400
-    """Check for data type"""
+
+    if not all(field in data for field in ["title", "organizer",\
+     "images", "location", "happening_on", "tags"]):
+        return jsonify({"status": 400,\
+         "message": "Please fill in all the required input fields"}), 400
+
 
     title = data.get('title')
-    createdOn = data.get('time')
+    created_on = data.get('time')
     organizer = data.get('organizer')
     images = data.get("images")
     location = data.get('location')
-    happeningOn = data.get('happeningOn')
+    happening_on = data.get('happening_on')
     tags = data.get('tags')
 
-    meet = jsonify(meetups.create_meetup(title, createdOn, organizer,
-                                         images, location, happeningOn, tags))
+    meet = jsonify(meetups.create_meetup(title, created_on,\
+     organizer, images, location, happening_on, tags))
     meet.status_code = 201
-    return meet
 
-
-@v1_meetup_blueprint.route('/meetups', methods=['GET'])
-def getall():
-    """ endpoint to fetch all meetups """
-
-    data = meetups.getall_meetups()
-    if data:
-        return make_response(jsonify({
-            "message": "Success",
-            "meetups": data
-        }), 200)
-    return make_response(jsonify({'message': 'Meetup not found'}), 404)
-
-
-@v1_meetup_blueprint.route('/meetups/<int:meetup_id>', methods=['GET'])
-def get_one_meetup(meetup_id):
-    """querry meetups by id"""
-    meetup = meetups.getone_meetup(meetup_id)
-    if meetup:
-        return make_response(jsonify({
-            "message": "Success",
-            "meetup": meetup
-        })), 200
-    return make_response(jsonify({'message': 'Meetup not found'}), 404)
-
-
-@v1_meetup_blueprint.route('/meetups/<int:meetup_id>/rsvp', methods=['POST'])
-def rsvp_meetup(meetup_id):
-    """ endpoint for rsvp meetup """
-    data = request.get_json()
-    if not data:
-        return jsonify({"message": "Data set cannot be empty"}), 400
-
-    meetup = meetups.getone_meetup(meetup_id)
-    if meetup:
-        user_id = data.get('user_id')
-        meetup_id = meetup_id
-        response = data.get('response')
-
-        res = rsvp.post_rsvp(user_id, meetup_id, response)
-
-        return res
-    return make_response(jsonify({'message': 'Meetup not found'}), 404)
+    return jsonify(meet, {
+        "status": 201,
+        "message": "Meetup added successfuly"
+    }), 201
