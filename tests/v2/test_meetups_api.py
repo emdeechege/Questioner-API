a6cc2e8951b8e78ee8bcaeed2ff9e4c59fd36f3d
Tom Chege <emdeechege@gmail.com>
2019-01-23 04:47:23 +0300

    [finishes #163378837] meetup tests passing

--- tests/v2/test_meetups_api.py
+++ tests/v2/test_meetups_api.py
@@ -35,13 +35,14 @@ class TestMeetup(unittest.TestCase):
             self.db.close()
 
     def test_create_meetup(self):
+        """creates new meetup"""
         res = self.client.post(
             "api/v2/meetups", data=json.dumps(self.meetup), content_type='application/json')
         res_data = json.loads(res.data.decode())
 
         self.assertEqual(res.status_code, 201)
         self.assertIn("Python Hackerthon", str(res_data))
-        
+
 
     def test_submit_empty_meetup_fields(self):
         """check for empty fields"""
@@ -51,6 +52,30 @@ class TestMeetup(unittest.TestCase):
         self.assertEqual(result["message"],
                          "Please fill in all the required input fields")
         self.assertEqual(response.status_code, 400)
+
+    def test_getall_meetups(self):
+        """ tests fetching all meetups"""
+        all_meetups = self.client.get("api/v2/meetups")
+
+        result = json.loads(all_meetups.data.decode())
+        self.assertEqual(result["message"], "Success")
+        self.assertEqual(all_meetups.status_code, 200)
+
+    def test_getone_meetup(self):
+        """ tests fetching of one meetup endpoint """
+
+        one_meetup = self.client.post(
+            "api/v2/meetups", data=json.dumps(self.meetup), content_type='application/json')
+        one_meetup_data = json.loads(one_meetup.data.decode())
+        self.assertIn("Python Hackerthon", str(one_meetup_data))
+        self.assertEqual(one_meetup.status_code, 201)
+
+    def test_get_no_meetup(self):
+        """ tests when meetup does not exist """
+        response = self.client.get("api/v2/meetups/16")
+        res = json.loads(response.data.decode())
+        self.assertEqual(res["message"], "Meetup not found")
+        self.assertEqual(response.status_code, 404)
 
 
 
