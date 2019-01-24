cf21ebc57d738c344983850a4710b977d93bf3f8
Tom Chege <emdeechege@gmail.com>
2019-01-24 05:13:19 +0300

    [start #163423896] creates decorator for login required

--- app/api/v2/models/basemodels.py
+++ app/api/v2/models/basemodels.py
@@ -1,6 +1,7 @@
 import jwt
+from functools import wraps
 from datetime import datetime, timedelta
-from flask import jsonify, make_response
+from flask import jsonify, make_response, request
 from instance.config import Config
 
 
@@ -31,19 +32,34 @@ class BaseModels(object):
     def generate_auth_token(username):
         """ Generate auth token """
         try:
-            payload = {'exp': datetime.utcnow() + timedelta(days=0, seconds=180),
+            payload = {'exp': datetime.utcnow() + timedelta(days=1, seconds=180),
                        'iat': datetime.utcnow(), 'sub': username}
             return jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
         except Exception as e:
             return e
 
-    @staticmethod
-    def verify_auth_token(auth_token):
-        """Verify auth token """
-        try:
-            payload = jwt.decode(auth_token, SECRET_KEY)
-            return payload['sub']
-        except jwt.ExpiredSignatureError:
-            return 'Token expired, login again'
-        except jwt.InvalidTokenError:
-            return 'Invalid token, login'
+def login_required(f):
+    """ User authentication function """
+    @wraps(f)
+    def authenticate(*args, **kwargs):
+        """ decorator for login authentication """
+        if 'Authorization' in request.headers:
+            token = request.headers['Authorization']
+            try:
+                payload = jwt.decode(
+                    token, SECRET_KEY,algorithm='HS256')
+            except jwt.ExpiredSignatureError:
+                return jsonify({
+                    'status': 401,
+                    'error': 'Your session has expired. Please login again'
+                }), 401
+        user = payload['sub']
+
+        if user:
+            return f(*args,**kwargs)
+        return jsonify({
+            "status": 404,
+            "error": "User not found"
+        }), 404
+
+    return authenticate
